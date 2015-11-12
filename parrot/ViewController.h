5dd037c3a52382316335c0e9df39adb2a4439192
purpen <purpen.w@gmail.com>
2015-11-12 10:25:00 +0800

    init code.

---
+++ parrot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  parrot
+//
+//  Created by xiaoyi on 15/11/12.
+//  Copyright © 2015年 taihuoniao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ parrot/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  parrot
+//
+//  Created by xiaoyi on 15/11/12.
+//  Copyright © 2015年 taihuoniao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
