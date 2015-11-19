db2e2c7ba99713125f85fc37d7f9858c898f77f5
purpen <purpen.w@gmail.com>
2015-11-19 13:02:11 +0800

    update custom line label.

--- parrot/FBLineLabel.m
+++ parrot/FBLineLabel.m
@@ -10,12 +10,20 @@
 
 @implementation FBLineLabel
 
-/*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
-    // Drawing code
+    /*
+     1.获取到上下文CGContextRef
+     2.设置画线的起点位置
+     3.画线，并画到另一个点的位置
+     4.渲染到屏幕上面
+     */
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextMoveToPoint(context, 0, rect.size.height/2);
+    CGContextAddLineToPoint(context, rect.size.width, rect.size.height/2);
+    CGContextStrokePath(context);
 }
-*/
+
 
 @end
